09ae224ace6f5da9be11510966f170aa20ce4220
Kota <zero.ans.k2017@gmail.com>
2017-09-20 08:48:51 +0000

    Add validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+    validates :title ,length: {maximum: 32}, presence: true
+    validates :message ,length: {maximum: 256} ,presence: true
+    validates :name ,length: {maximum: 32}
 end
